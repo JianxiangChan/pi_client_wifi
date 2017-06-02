5c4289147a55fc75b960372cca1da159a5f172ce
JianxiangChan <15651898806@163.com>
2017-06-02 13:53:10 +0000

    add new file

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-src_dir=/mnt/hgfs/winshare/pi_client_wifi/src  
+src_dir=/mnt/hgfs/winshare/pi_client_wifi/src/  
 dest_dir=/home/pi/pi_client_wifi/src  
 host=192.168.199.221
 username=root  
@@ -10,7 +10,7 @@ if [ ! -d $dest_dir ]; then
   mkdir -p $dest_dir  
 else  
   rm -rf $dest_dir  
-  mkdir -p $dest_dir  
+ # mkdir -p $dest_dir  
 fi  
   
 # 将远程服务器上的文件拷贝到本机  
